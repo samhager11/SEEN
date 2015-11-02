f23c32f20ee24fddb0b09aeaa4291ea02f5ca48d
Sam Hager <sam.hager11@gmail.com>
2015-11-01 19:49:16 -0800

    added static pages controller with views for about, how_to, and faq.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def how_to
+  end
+
+  def faq
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/how_to'
+
+  get 'static_pages/faq'
+
 #User Routes/////////////////
 
   get 'users/index'
